2aed6f81c8dab98d423e6e63c68a7d29425863e4
Gustas Kurtkus <gusteakask@gmail.com>
2018-01-23 22:34:19 +0000

    Created a middleware for administration system and added 'admin' field to user's model.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,10 +14,15 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 250)->unique();
             $table->string('password');
+            $table->integer('admin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Middleware\CheckAdmin;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,3 +10,17 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => CheckAdmin::class, 'prefix' => 'admin'], function() {
+
+    Route::get('/', 'AdminController@index')->name('admin.index');
+
+});
+
+
+
+
+
+
+
+
